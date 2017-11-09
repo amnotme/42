59fb9ae241e884127796d806bcf70f7cddee7ad7
Leopoldo Hernandez <lhernand@e1z2r1p12.42.us.org>
2017-11-09 13:30:28 -0800

    Added description for ft_isprint.c

--- libft/ft_isprint.c
+++ libft/ft_isprint.c
@@ -6,11 +6,20 @@
 /*   By: lhernand <lhernand@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/25 09:38:41 by lhernand          #+#    #+#             */
-/*   Updated: 2017/09/25 09:38:43 by lhernand         ###   ########.fr       */
+/*   Updated: 2017/11/09 13:29:57 by lhernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+/*
+** The ft_isprint() function tests for any printing character, including space
+** (` ').  The value of the argument must be representable as an unsigned char
+** or the value of EOF.
+**
+** The ft_isprint() function returns zero if the character tests false and
+** returns non-zero if the character tests true.
+*/
 
 int		ft_isprint(int c)
 {
