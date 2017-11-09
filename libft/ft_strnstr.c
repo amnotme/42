4b7faca2c753385c2dc42c652dc40b97b42e68ab
Leopoldo Hernandez <lhernand@e1z2r1p12.42.us.org>
2017-11-09 12:45:25 -0800

    Added description for ft_strnstr.c

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -6,11 +6,21 @@
 /*   By: lhernand <lhernand@student.us.org>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/27 21:12:49 by lhernand          #+#    #+#             */
-/*   Updated: 2017/10/21 20:00:42 by lhernand         ###   ########.fr       */
+/*   Updated: 2017/11/09 12:45:11 by lhernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+/*
+** The ft_strnstr() function locates the first occurrence of the null-terminated
+** string little in the string big, where not more than len characters are
+** searched.  Characters that appear after a `\0' character are not searched.
+**
+** If little is an empty string, big is returned; if little occurs nowhere in
+** big, NULL is returned; otherwise a pointer to the first character of the
+** first occurrence of little is returned.
+*/
 
 char		*ft_strnstr(const char *big, const char *little, size_t l)
 {
