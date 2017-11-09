4040311567010177ba5b5afd9a74ad5ae6d42eae
Leopoldo Hernandez <lhernand@e1z2r1p12.42.us.org>
2017-11-09 15:48:41 -0800

    Added description for ft_strsub.c

--- libft/ft_strsub.c
+++ libft/ft_strsub.c
@@ -6,11 +6,21 @@
 /*   By: lhernand <lhernand@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/18 20:59:01 by lhernand          #+#    #+#             */
-/*   Updated: 2017/10/21 02:05:53 by lhernand         ###   ########.fr       */
+/*   Updated: 2017/11/09 15:48:26 by lhernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+/*
+** Allocates (with malloc(3)) and returns a “fresh” substring
+** from the string given as argument. The substring begins at
+** indexstart and is of size len. If start and len aren’t refering
+** to a valid substring, the behavior is undefined. If the
+** allocation fails, the function returns NULL.
+**
+**  The substring.
+*/
 
 char				*ft_strsub(char const *s, unsigned int start, size_t len)
 {
