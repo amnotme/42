0ba04362c44888c5ad9409636b64bab9db37773e
Leopoldo Hernandez <lhernand@e1z2r1p12.42.us.org>
2017-11-09 01:50:00 -0800

    Added description to ft_strncpy.c

--- libft/ft_strncpy.c
+++ libft/ft_strncpy.c
@@ -6,11 +6,22 @@
 /*   By: lhernand <lhernand@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/25 09:37:22 by lhernand          #+#    #+#             */
-/*   Updated: 2017/10/21 20:00:11 by lhernand         ###   ########.fr       */
+/*   Updated: 2017/11/09 01:49:42 by lhernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+/*
+** The ft_strncpy() function copies at most len characters from src into dst.
+** If src is less than len characters long, the remainder of dst is filled 
+** with `\0' characters.  Otherwise, dst is not terminated.
+**
+** The source and destination strings should not overlap, as the behavior
+** is undefined.
+**
+** The ft_strcpy() and ft_strncpy() functions return dst.
+*/
 
 char		*ft_strncpy(char *dst, char const *src, size_t len)
 {
