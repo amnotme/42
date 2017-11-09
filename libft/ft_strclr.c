b358f119db470029424fddc950da8d5011d0a391
Leopoldo Hernandez <lhernand@e1z2r1p12.42.us.org>
2017-11-09 13:50:08 -0800

    Added ft_strclr.c description

--- libft/ft_strclr.c
+++ libft/ft_strclr.c
@@ -6,11 +6,15 @@
 /*   By: lhernand <lhernand@student.us.org>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/17 21:58:13 by lhernand          #+#    #+#             */
-/*   Updated: 2017/10/17 22:02:46 by lhernand         ###   ########.fr       */
+/*   Updated: 2017/11/09 13:49:42 by lhernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+/*
+** Sets every character of the string to the value ’\0’.
+*/
 
 void		ft_strclr(char *s)
 {
--- libft/ft_strdel.c
+++ libft/ft_strdel.c
@@ -6,11 +6,16 @@
 /*   By: lhernand <lhernand@student.us.org>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/17 21:53:53 by lhernand          #+#    #+#             */
-/*   Updated: 2017/10/17 21:57:44 by lhernand         ###   ########.fr       */
+/*   Updated: 2017/11/09 13:48:49 by lhernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+/*
+** Takes as a parameter the address of a string that need to be
+** freed with free(3), then sets its pointer to NULL
+*/
 
 void		ft_strdel(char **as)
 {
