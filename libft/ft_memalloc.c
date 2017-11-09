b4b39e98a7047a24eef44ef3bb88e949a5c47357
Leopoldo Hernandez <lhernand@e1z2r1p12.42.us.org>
2017-11-09 13:42:28 -0800

    Added description for ft_memalloc.c

--- libft/ft_memalloc.c
+++ libft/ft_memalloc.c
@@ -6,11 +6,19 @@
 /*   By: lhernand <lhernand@student.us.org>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/17 20:51:45 by lhernand          #+#    #+#             */
-/*   Updated: 2017/10/17 21:39:53 by lhernand         ###   ########.fr       */
+/*   Updated: 2017/11/09 13:42:11 by lhernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+/*
+** Allocates (with malloc(3)) and returns a “fresh” memory
+** area. The memory allocated is initialized to 0. If the allocation
+** fails, the function returns NULL.
+**
+** Returns the allocated memory area.
+*/
 
 void		*ft_memalloc(size_t size)
 {
