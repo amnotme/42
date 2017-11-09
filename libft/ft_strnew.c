4944de508f1b3f9e077f88cad68a268eae39929a
Leopoldo Hernandez <lhernand@e1z2r1p12.42.us.org>
2017-11-09 13:47:06 -0800

    Added description for ft_strnew.c

--- libft/ft_strnew.c
+++ libft/ft_strnew.c
@@ -6,11 +6,19 @@
 /*   By: lhernand <lhernand@student.us.org>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/17 21:46:36 by lhernand          #+#    #+#             */
-/*   Updated: 2017/10/17 21:53:42 by lhernand         ###   ########.fr       */
+/*   Updated: 2017/11/09 13:46:40 by lhernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+/*
+** Allocates (with malloc(3)) and returns a “fresh” string ending
+** with ’\0’. Each character of the string is initialized at
+** ’\0’. If the allocation fails the function returns NULL.
+**
+** The string allocated and initialized to 0.
+*/
 
 void		*ft_strnew(size_t size)
 {
