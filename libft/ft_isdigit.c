9a1c6fc3958f25c8bc4b4f13cdb141b28c71da9a
Leopoldo Hernandez <lhernand@e1z2r1p12.42.us.org>
2017-11-09 13:24:29 -0800

    Added description for ft_isdigit.c

--- libft/ft_isdigit.c
+++ libft/ft_isdigit.c
@@ -6,11 +6,18 @@
 /*   By: lhernand <lhernand@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/25 09:35:52 by lhernand          #+#    #+#             */
-/*   Updated: 2017/09/25 09:35:54 by lhernand         ###   ########.fr       */
+/*   Updated: 2017/11/09 13:23:57 by lhernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+/*
+** The ft_isdigit() function tests for a decimal digit character.
+**
+** The ft_isdigit() function returns zero if the character tests false and
+** return non-zero if the character tests true.
+*/
 
 int		ft_isdigit(int c)
 {
