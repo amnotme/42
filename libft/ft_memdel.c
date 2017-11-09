d9139798219140713f088bb82043dca94f3c860d
Leopoldo Hernandez <lhernand@e1z2r1p12.42.us.org>
2017-11-09 13:44:20 -0800

    Added description to ft_memdel.c

--- libft/ft_memdel.c
+++ libft/ft_memdel.c
@@ -6,11 +6,16 @@
 /*   By: lhernand <lhernand@student.us.org>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/17 20:59:57 by lhernand          #+#    #+#             */
-/*   Updated: 2017/10/17 21:39:51 by lhernand         ###   ########.fr       */
+/*   Updated: 2017/11/09 13:44:02 by lhernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+/*
+** Takes as a parameter the address of a memory area that needs
+** to be freed with free(3), then puts the pointer to NULL.
+*/
 
 void		ft_memdel(void **ap)
 {
