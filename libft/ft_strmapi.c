63f328f290e92a10c4f4431b96e850daa4d5bd51
Leopoldo Hernandez <lhernand@e1z2r1p12.42.us.org>
2017-11-09 14:01:27 -0800

    Added description for ft_strmapi.c

--- libft/ft_strmapi.c
+++ libft/ft_strmapi.c
@@ -6,11 +6,20 @@
 /*   By: lhernand <lhernand@student.us.org>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/17 22:22:22 by lhernand          #+#    #+#             */
-/*   Updated: 2017/10/21 03:58:56 by lhernand         ###   ########.fr       */
+/*   Updated: 2017/11/09 14:01:10 by lhernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+/*
+** Applies the function f to each character of the string passed
+** as argument by giving its index as first argument to create a
+** “fresh” new string (with malloc(3)) resulting from the successive
+** applications of f.
+**
+** Returns the “fresh” string created from the successive applications of f.
+*/
 
 char		*ft_strmapi(const char *s, char (*f)(unsigned int, char))
 {
