4098343cd1659c6b9d4f795ef964f1080e0738c0
Leopoldo Hernandez <lhernand@e1z2r1p12.42.us.org>
2017-11-09 15:44:05 -0800

    Added description to ft_strequ.c

--- libft/ft_strequ.c
+++ libft/ft_strequ.c
@@ -6,11 +6,18 @@
 /*   By: lhernand <lhernand@student.us.org>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/17 22:34:45 by lhernand          #+#    #+#             */
-/*   Updated: 2017/10/17 22:45:05 by lhernand         ###   ########.fr       */
+/*   Updated: 2017/11/09 15:43:53 by lhernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+/*
+** Lexicographical comparison between s1 and s2. If the 2
+** strings are identical the function returns 1, or 0 otherwise
+**
+** 1 or 0 according to if the 2 strings are identical or not.
+*/
 
 int		ft_strequ(const char *s1, const char *s2)
 {
