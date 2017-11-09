679ec546d9b9ea29b6cab94330ab1ff27c6ac79e
Leopoldo Hernandez <lhernand@e1z2r1p12.42.us.org>
2017-11-09 12:54:01 -0800

    Added description to ft_strncmp.c

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -6,11 +6,25 @@
 /*   By: lhernand <lhernand@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/25 09:37:45 by lhernand          #+#    #+#             */
-/*   Updated: 2017/10/21 19:59:45 by lhernand         ###   ########.fr       */
+/*   Updated: 2017/11/09 12:53:45 by lhernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+/*
+** The ft_strcmp() and ft_strncmp() functions lexicographically compare the 
+** null-terminated strings s1 and s2.
+**
+** The ft_strncmp() function compares not more than n characters.  Because
+** ft_strncmp() is designed for comparing strings rather than binary data,
+** characters that appear after a `\0' character are not compared.
+**
+** The ft_strcmp() and ft_strncmp() functions return an integer greater than,
+** equal to, or less than 0, according as the string s1 is greater than,
+** equal to, or less than the string s2.  The comparison is done using
+** unsigned characters, so that `\200' is greater than `\0'.
+*/
 
 int					ft_strncmp(char const *s1, char const *s2, size_t n)
 {
