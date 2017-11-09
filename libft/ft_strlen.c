f35431e48cda39c4ed0c8603c0858d4e49e40cdd
Leopoldo Hernandez <lhernand@e1z2r1p12.42.us.org>
2017-11-09 01:38:00 -0800

    Added description to ft_strlen.c

--- libft/ft_strlen.c
+++ libft/ft_strlen.c
@@ -6,11 +6,21 @@
 /*   By: lhernand <lhernand@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/25 09:42:54 by lhernand          #+#    #+#             */
-/*   Updated: 2017/09/25 09:42:58 by lhernand         ###   ########.fr       */
+/*   Updated: 2017/11/09 01:37:39 by lhernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "string.h"
+
+/*
+** The ft_strlen() function computes the length of the string s. ft_strnlen() 
+** function attempts to compute the length of s, but never scans beyond the 
+** first maxlen bytes of s.
+**
+** The ft_strlen() function returns the number of characters that precede 
+** the terminating NUL character.  The ft_strnlen() function returns either
+** the same result as ft_strlen() or maxlen, whichever is smaller. 
+*/
 
 size_t			ft_strlen(char const *s)
 {
