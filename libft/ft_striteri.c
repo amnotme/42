460638d00e7dae1fdc1caa5a7d7d7aebc3629237
Leopoldo Hernandez <lhernand@e1z2r1p12.42.us.org>
2017-11-09 13:55:01 -0800

    Added description for ft_striteri.c

--- libft/ft_striteri.c
+++ libft/ft_striteri.c
@@ -6,11 +6,17 @@
 /*   By: lhernand <lhernand@student.us.org>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/17 22:09:30 by lhernand          #+#    #+#             */
-/*   Updated: 2017/10/17 22:13:20 by lhernand         ###   ########.fr       */
+/*   Updated: 2017/11/09 13:54:31 by lhernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+/*
+** Applies the function f to each character of the string passed
+** as argument, and passing its index as first argument. Each
+** character is passed by address to f to be modified if necessary.
+*/
 
 void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 {
