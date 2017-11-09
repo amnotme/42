8f81315eeff9af16b047626ae4972ad1e1fd20f1
Leopoldo Hernandez <lhernand@e1z2r1p12.42.us.org>
2017-11-09 13:52:22 -0800

    Added description to ft_striter.c

--- libft/ft_striter.c
+++ libft/ft_striter.c
@@ -6,11 +6,17 @@
 /*   By: lhernand <lhernand@student.us.org>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/17 22:05:29 by lhernand          #+#    #+#             */
-/*   Updated: 2017/10/17 22:09:00 by lhernand         ###   ########.fr       */
+/*   Updated: 2017/11/09 13:52:04 by lhernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+/*
+** Applies the function f to each character of the string passed
+** as argument. Each character is passed by address to f to be
+** modified if necessary.
+*/
 
 void		ft_striter(char *s, void (*f)(char *))
 {
