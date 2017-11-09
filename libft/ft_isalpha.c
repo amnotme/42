9d3d7e62b84abd24b1ec5cdaaa7241efc0a8aa27
Leopoldo Hernandez <lhernand@e1z2r1p12.42.us.org>
2017-11-09 13:00:40 -0800

    Added description to ft_isalpha.c

--- libft/ft_isalpha.c
+++ libft/ft_isalpha.c
@@ -6,11 +6,20 @@
 /*   By: lhernand <lhernand@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/25 09:42:22 by lhernand          #+#    #+#             */
-/*   Updated: 2017/09/25 09:42:24 by lhernand         ###   ########.fr       */
+/*   Updated: 2017/11/09 13:00:18 by lhernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+/*
+** The ft_isalpha() function tests for any character for which ft_isupper() or
+** ft_islower() is true.  The value of the argument must be representable as
+** an unsigned char or the value of EOF.
+**
+** The ft_isalpha() function returns zero if the character tests false and
+** returns non-zero if the character tests true.
+*/
 
 int			ft_isalpha(int c)
 {
