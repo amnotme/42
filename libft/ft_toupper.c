d7e123263ff6638309d3369a6531c9f6d2ee08f5
Leopoldo Hernandez <lhernand@e1z2r1p12.42.us.org>
2017-11-09 13:32:45 -0800

    Added description to ft_toupper.c

--- libft/ft_toupper.c
+++ libft/ft_toupper.c
@@ -6,11 +6,20 @@
 /*   By: lhernand <lhernand@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/25 09:43:11 by lhernand          #+#    #+#             */
-/*   Updated: 2017/09/25 09:43:13 by lhernand         ###   ########.fr       */
+/*   Updated: 2017/11/09 13:32:22 by lhernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+/*
+** The ft_toupper() function converts a lower-case letter to the corresponding
+** upper-case letter.  The argument must be representable as an unsigned char
+** or the value of EOF.
+**
+** If the argument is a lower-case letter, the ft_toupper() function returns
+** the corresponding upper-case letter if there is one; otherwise, the
+** argument is returned unchanged.
+*/
 
 int		ft_toupper(int c)
 {
