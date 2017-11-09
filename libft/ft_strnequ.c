a271feacab618922292a6b8ed779e8e42672046b
Leopoldo Hernandez <lhernand@e1z2r1p12.42.us.org>
2017-11-09 15:45:16 -0800

    Added description for ft_strnequ.c

--- libft/ft_strnequ.c
+++ libft/ft_strnequ.c
@@ -6,11 +6,19 @@
 /*   By: lhernand <lhernand@student.us.org>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/17 22:47:54 by lhernand          #+#    #+#             */
-/*   Updated: 2017/10/21 04:02:24 by lhernand         ###   ########.fr       */
+/*   Updated: 2017/11/09 15:45:00 by lhernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+/*
+** Lexicographical comparison between s1 and s2 up to n characters
+** or until a ’\0’ is reached. If the 2 strings are identical,
+** the function returns 1, or 0 otherwise
+**
+** 1 or 0 according to if the 2 strings are identical or not.
+*/
 
 int		ft_strnequ(const char *s1, const char *s2, size_t n)
 {
