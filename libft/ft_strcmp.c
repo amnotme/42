b897bfec14c3fdd86b6c11592d5db8dfc75a4693
Leopoldo Hernandez <lhernand@e1z2r1p12.42.us.org>
2017-11-09 12:51:09 -0800

    Added description for ft_strcmp.c

--- libft/ft_strcmp.c
+++ libft/ft_strcmp.c
@@ -6,9 +6,19 @@
 /*   By: lhernand <lhernand@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/25 09:39:08 by lhernand          #+#    #+#             */
-/*   Updated: 2017/10/21 01:31:19 by lhernand         ###   ########.fr       */
+/*   Updated: 2017/11/09 12:50:56 by lhernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/*
+** The ft_strcmp()function lexicographically compares the null-terminated
+** strings s1 and s2.
+**
+** The ft_strcmp() function returns an integer greater than, equal to, or less
+** than 0, according as the string s1 is greater than, equal to, or less than
+** the string s2.  The comparison is done using unsigned characters, so that
+** `\200' is greater than `\0'.
+*/
 
 int				ft_strcmp(char const *s1, char const *s2)
 {
