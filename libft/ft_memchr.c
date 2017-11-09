6f4c51f4fdcae164f53295e7617a8a15366a8ee9
Leopoldo Hernandez <lhernand@e1z2r1p12.42.us.org>
2017-11-09 01:31:20 -0800

    Added description to ft_memchr.c

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -6,11 +6,19 @@
 /*   By: lhernand <lhernand@student.us.org>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/28 16:34:11 by lhernand          #+#    #+#             */
-/*   Updated: 2017/10/21 02:04:00 by lhernand         ###   ########.fr       */
+/*   Updated: 2017/11/09 01:31:05 by lhernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+/*
+** The ft_memchr() function locates the first occurrence of c (converted to 
+** an unsigned char) in string s.
+**
+** The ft_memchr() function returns a pointer to the byte located, or NULL 
+** if no such byte exists within n bytes.
+*/
 
 void					*ft_memchr(const void *s, int c, size_t n)
 {
