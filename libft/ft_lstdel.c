d3d91c08ba11d039097566c1c4f5e117bdc2b126
Leopoldo Hernandez <lhernand@e1z2r1p12.42.us.org>
2017-11-09 17:26:45 -0800

    Added description for ft_lstdel and ft_lstdelone

--- libft/ft_lstdel.c
+++ libft/ft_lstdel.c
@@ -6,11 +6,19 @@
 /*   By: lhernand <lhernand@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/20 21:24:46 by lhernand          #+#    #+#             */
-/*   Updated: 2017/10/20 21:30:16 by lhernand         ###   ########.fr       */
+/*   Updated: 2017/11/09 17:26:16 by lhernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+/*
+** Takes as a parameter the adress of a pointer to a link and
+** frees the memory of this link and every successors of that link
+** using the functions del and free(3). Finally the pointer to
+** the link that was just freed must be set to NULL (quite similar
+** to the function ft_memdel from the mandatory part).
+*/
 
 void		ft_lstdel(t_list **alst, void (*del)(void *, size_t))
 {
