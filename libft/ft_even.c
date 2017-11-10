85cd5c28a30dc207fd986afa541140519c14d401
Leopoldo Hernandez <lhernand@e1z2r1p12.42.us.org>
2017-11-09 16:50:20 -0800

    Added description for ft_even and ft_odd

--- libft/ft_even.c
+++ libft/ft_even.c
@@ -6,9 +6,13 @@
 /*   By: lhernand <lhernand@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/25 10:48:13 by lhernand          #+#    #+#             */
-/*   Updated: 2017/10/21 03:56:47 by lhernand         ###   ########.fr       */
+/*   Updated: 2017/11/09 16:49:12 by lhernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/*
+**	Takes a valid integer and returns 1 if the number is even otherwise 0.
+*/
 
 int		ft_even(int num)
 {
--- libft/ft_odd.c
+++ libft/ft_odd.c
@@ -6,9 +6,14 @@
 /*   By: lhernand <lhernand@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/25 13:02:12 by lhernand          #+#    #+#             */
-/*   Updated: 2017/10/21 01:28:31 by lhernand         ###   ########.fr       */
+/*   Updated: 2017/11/09 16:49:59 by lhernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/*
+** This ft_odd() function takes a valid integer and returns 1 if it's an odd
+** number, otherwise it returns 0.
+*/
 
 int		ft_odd(int num)
 {
