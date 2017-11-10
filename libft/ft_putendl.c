b80c6be117e12209f034151c0fdf8fb633d5ef81
Leopoldo Hernandez <lhernand@e1z2r1p12.42.us.org>
2017-11-09 16:27:21 -0800

    Added description ft_putendl.c

--- libft/ft_putendl.c
+++ libft/ft_putendl.c
@@ -6,11 +6,15 @@
 /*   By: lhernand <lhernand@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/25 09:42:36 by lhernand          #+#    #+#             */
-/*   Updated: 2017/09/25 13:46:00 by lhernand         ###   ########.fr       */
+/*   Updated: 2017/11/09 16:27:04 by lhernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+/*
+** Outputs the string s to the standard output followed by a ’\n’.
+*/
 
 void	ft_putendl(char const *s)
 {
