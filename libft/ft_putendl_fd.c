bd917a75795c993778cbc343d5faafac3c446af1
Leopoldo Hernandez <lhernand@e1z2r1p12.42.us.org>
2017-11-09 16:30:47 -0800

    Added description for ft_putendl_fd.c

--- libft/ft_putendl_fd.c
+++ libft/ft_putendl_fd.c
@@ -6,11 +6,15 @@
 /*   By: lhernand <lhernand@student.us.org>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/27 22:26:39 by lhernand          #+#    #+#             */
-/*   Updated: 2017/09/27 22:44:37 by lhernand         ###   ########.fr       */
+/*   Updated: 2017/11/09 16:30:33 by lhernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+/*
+** Outputs the string s to the file descriptor fd followed by a ’\n’.
+*/
 
 void		ft_putendl_fd(char const *s, int fd)
 {
