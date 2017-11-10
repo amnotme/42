cb5aa806646815d7ba855c71247734763f8f8381
Leopoldo Hernandez <lhernand@e1z2r1p12.42.us.org>
2017-11-09 17:32:17 -0800

    Added descriptions for ft_lstadd and ft_lstiter

--- libft/ft_lstadd.c
+++ libft/ft_lstadd.c
@@ -6,11 +6,15 @@
 /*   By: lhernand <lhernand@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/20 21:31:37 by lhernand          #+#    #+#             */
-/*   Updated: 2017/10/20 21:36:31 by lhernand         ###   ########.fr       */
+/*   Updated: 2017/11/09 17:31:31 by lhernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+/*
+** Adds the element new at the beginnning of the list.
+*/
 
 void		ft_lstadd(t_list **alst, t_list *new)
 {
