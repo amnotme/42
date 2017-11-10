201cab25b6fc0f447b5a024da04a21b57a23e9d8
Leopoldo Hernandez <lhernand@e1z2r1p12.42.us.org>
2017-11-09 16:43:54 -0800

    Added description for true_false.c

--- libft/true_false.c
+++ libft/true_false.c
@@ -6,11 +6,16 @@
 /*   By: lhernand <lhernand@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/25 13:13:35 by lhernand          #+#    #+#             */
-/*   Updated: 2017/10/21 19:52:45 by lhernand         ###   ########.fr       */
+/*   Updated: 2017/11/09 16:43:23 by lhernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+/*
+** The true_false() will output 'FALSE' if the number passed is 0, 'TRUE' if
+** the number is 1 and 'Did not return 1 or 0 for any other number.
+*/
 
 char		*true_false(unsigned int num)
 {
