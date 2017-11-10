81aa86a59f9e85280cc3c9786ff7f9719956c7fd
Leopoldo Hernandez <lhernand@e1z2r1p12.42.us.org>
2017-11-09 16:01:13 -0800

    Added description for ft_iswpace

--- libft/ft_iswspace.c
+++ libft/ft_iswspace.c
@@ -6,11 +6,16 @@
 /*   By: lhernand <lhernand@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/18 21:17:13 by lhernand          #+#    #+#             */
-/*   Updated: 2017/10/18 21:29:19 by lhernand         ###   ########.fr       */
+/*   Updated: 2017/11/09 16:00:53 by lhernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+/*
+** The ft_iswspace() function returns a non-zero number to indicate for a
+** truthy value
+*/
 
 int			ft_iswspace(int c)
 {
