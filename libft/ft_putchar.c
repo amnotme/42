9d943623c3f9476b4d5f27dd103cc7379c02e767
Leopoldo Hernandez <lhernand@e1z2r1p12.42.us.org>
2017-11-09 16:25:43 -0800

    Added description to ft_putchar.c

--- libft/ft_putchar.c
+++ libft/ft_putchar.c
@@ -6,11 +6,15 @@
 /*   By: lhernand <lhernand@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/25 09:40:34 by lhernand          #+#    #+#             */
-/*   Updated: 2017/09/25 13:46:07 by lhernand         ###   ########.fr       */
+/*   Updated: 2017/11/09 16:25:24 by lhernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+/*
+** Outputs the character c to the standard output.
+*/
 
 void		ft_putchar(char c)
 {
