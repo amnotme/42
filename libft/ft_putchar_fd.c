12a8fc18b56b70aa3006e9a628896ceb2371f9cd
Leopoldo Hernandez <lhernand@e1z2r1p12.42.us.org>
2017-11-09 16:29:26 -0800

    Added description for ft_putchar_fd.c

--- libft/ft_putchar_fd.c
+++ libft/ft_putchar_fd.c
@@ -6,11 +6,15 @@
 /*   By: lhernand <lhernand@student.us.org>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/27 21:34:22 by lhernand          #+#    #+#             */
-/*   Updated: 2017/10/21 03:55:51 by lhernand         ###   ########.fr       */
+/*   Updated: 2017/11/09 16:29:04 by lhernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+/*
+** Outputs the char c to the file descriptor fd.
+*/
 
 void	ft_putchar_fd(char c, int fd)
 {
