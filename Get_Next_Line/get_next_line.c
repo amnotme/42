f04806dfb3e1341ea2e85096fd8e696c582d340f
Leopoldo Hernandez <lhernand@e1z1r2p1.42.us.org>
2017-12-13 03:30:28 -0800

    removed libft... didn't need it

--- Get_Next_Line/get_next_line.c
+++ Get_Next_Line/get_next_line.c
@@ -3,31 +3,132 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lhernand <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: lhernand <lhernand@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/12/07 22:53:16 by lhernand          #+#    #+#             */
-/*   Updated: 2017/12/08 01:17:52 by lhernand         ###   ########.fr       */
+/*   Created: 2017/12/12 20:57:18 by lhernand          #+#    #+#             */
+/*   Updated: 2017/12/13 03:29:39 by lhernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-
-int 		get_next_line(const int fd, char **line)
+static char		*ft_strdup(const char *s1)
 {
+	size_t		i;
+	char		*s2;
 
+	i = 0;
+	while (s1[i])
+		i++;
+	if (!(s2 = (char *)malloc(sizeof(char) * (i + 1))))
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		s2[i] = s1[i];
+		i++;
+	}
+	s2[i] = '\0';
+	return (s2);
+}
+
+static char		*ft_strjoin(const char *s1, const char *s2)
+{
+	int			i;
+	int			j;
+	int			z;
+	char		*s3;
+
+	i = 0;
+	j = 0;
+	z = 0;
+	while (s1[i])
+		i++;
+	while (s2[j])
+		j++;
+	if (!s1 || !s2 || !(s3 = (char *)malloc(sizeof(char) * (i + j + 1))))
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (s1[i])
+		s3[z++] = s1[i++];
+	while (s2[j])
+		s3[z++] = s2[j++];
+	s3[z] = '\0';
+	return (s3);
+}
+
+static int		valid_line(char **stk, char **line)
+{
+	char		*chars_in_stk;
+	char		*tmp_stk;
+	int			pos;
+
+	pos = 0;
+	chars_in_stk = *stk;
+	while (chars_in_stk[pos] != '\n')
+	{
+		if (chars_in_stk[pos] == '\0')
+			return (0);
+		pos++;
+	}
+	tmp_stk = &chars_in_stk[pos];
+	*tmp_stk = '\0';
+	*line = ft_strdup(*stk);
+	*stk = ft_strdup(tmp_stk + 1);
 	return (1);
 }
 
-int			main(int argc, char **argv)
+static int		read_file(int fd, char *hp, char **stk, char **line)
 {
-	if (argc != 2)
-		ft_putchar('\n');
-	else
-		while ((get_next_line(fd, line) == 1))
+	int			ret;
+	char		*ptr;
+
+	while ((ret = read(fd, hp, BUFF_SIZE)) > 0)
+	{
+		hp[ret] = '\0';
+		if (*stk)
 		{
-			ft_putendl(line);
-			
+			ptr = *stk;
+			*stk = ft_strjoin(ptr, hp);
+			free(ptr);
+			ptr = NULL;
 		}
-	return (0);
+		else
+			*stk = ft_strdup(hp);
+		if (valid_line(stk, line))
+			break ;
+	}
+	if (ret > 0)
+		return (1);
+	return (ret);
+}
+
+int				get_next_line(const int fd, char **line)
+{
+	static char *stk[DESCRIPTORS];
+	char		*hp;
+	int			ret;
+	int			i;
+
+	i = 0;
+	if (!(hp = (char *)malloc(sizeof(char) * BUFF_SIZE + 1)) || fd < 0 || !line
+			|| fd > DESCRIPTORS || (read(fd, stk[fd], 0) < 0))
+		return (-1);
+	if (stk[fd])
+		if (valid_line(&stk[fd], line))
+			return (1);
+	while (i < BUFF_SIZE)
+		hp[i++] = '\0';
+	ret = read_file(fd, hp, &stk[fd], line);
+	free(hp);
+	if (stk[fd][0] == '\0' || stk[fd] == NULL || ret != 0)
+	{
+		if ((ret == 0) && (*line))
+			*line = NULL;
+		return (ret);
+	}
+	*line = stk[fd];
+	stk[fd] = NULL;
+	return (1);
 }
--- Get_Next_Line/get_next_line.h
+++ Get_Next_Line/get_next_line.h
@@ -3,22 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lhernand <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: lhernand <lhernand@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/12/07 22:15:50 by lhernand          #+#    #+#             */
-/*   Updated: 2017/12/08 01:18:31 by lhernand         ###   ########.fr       */
+/*   Created: 2017/12/13 02:28:51 by lhernand          #+#    #+#             */
+/*   Updated: 2017/12/13 03:22:47 by lhernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# include "./libft/libft.h"
 # include <fcntl.h>
 # include <sys/types.h>
 # include <sys/stat.h>
 # include <stdlib.h>
+# include <unistd.h>
 
-# define BUFF_SIZE 32
+# define BUFF_SIZE 8
+# define DESCRIPTORS 4864
 
 int			get_next_line(const int fd, char **line);
 
