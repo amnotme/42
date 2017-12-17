c75af452f7400585618a895a02f07a6bb29f9237
amnotme <amnotme@icloud.com>
2017-12-17 02:12:15 -0800

    added Makefiles

--- Fdf/main2.c
+++ Fdf/main2.c
@@ -6,7 +6,7 @@
 /*   By: leopoldohernandez <marvin@42.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/16 22:31:33 by leopoldoh         #+#    #+#             */
-/*   Updated: 2017/12/16 22:54:21 by leopoldoh        ###   ########.fr       */
+/*   Updated: 2017/12/17 02:03:06 by leopoldoh        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,13 @@ int                 key_hook(int keycode, t_env *e)
     ft_putstr("key pressed -> : ");
     ft_putnbr(keycode);
     ft_putchar('\n');
-    if (keycode == 65307)
+    if (keycode == 53)
+    {
+        mlx_destroy_window(e->mlx, e->win);
         exit (0);
-    (void)(e);
+    }
+    else if (keycode == 8)
+        mlx_clear_window(e->mlx, e->win);
     return (1);
 }
 
@@ -41,11 +45,13 @@ void                draw(void *mlx, void *win)
         x = 200;
         while (x < 400)
         {
-            mlx_pixel_put(mlx, win, x, y, 0xFF00FF);
+           // mlx_pixel_put(mlx, win, x, y, 0xFF00FF);
             x++;
+            if (x % 15 == 0 && y % 15 == 0)
+                mlx_pixel_put(mlx, win, x , y, 0x00FF00);
         }
         y++;
-        usleep(500);
+        usleep(100);
     }
 
 }
@@ -53,16 +59,27 @@ void                draw(void *mlx, void *win)
 int                     main(int argc, char **argv)
 {
     t_env e;
+    int x;
+    int y;
 
+    x = 0;
+    y = 0;
     e.mlx = mlx_init();
     if (argc == 1)
         e.win = mlx_new_window(e.mlx, 500, 500, "fdf");
-    else if (argc == 2)
-        e.win = mlx_new_window(e.mlx, ft_atoi(argv[1]), ft_atoi(argv[2]), "fdf");
+    else if (argc == 3)
+    {
+        if (argv[1] && argv[2])
+        {
+            x = ft_atoi(argv[1]);
+            y = ft_atoi(argv[2]);
+        }
+        e.win = mlx_new_window(e.mlx, x, y, "fdf");
+    }
     else
         return (-1);
-    //draw(e.mlx, e.win);
-
+    draw(e.mlx, e.win);
+    mlx_key_hook(e.win, key_hook, &e);
     mlx_loop(e.mlx);
     return (0);
 }
