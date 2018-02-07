dae318b5a564fea1e3780f97437bfb85b1d7fc17
amnotme <amnotme@icloud.com>
2018-02-07 10:44:16 -0800

    first commit for wolf3d

---
+++ Wolf3D/wolf3d.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   wolf3d.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lhernand <lhernand@student.42.us.or>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/02/07 10:38:50 by lhernand          #+#    #+#             */
+/*   Updated: 2018/02/07 10:40:43 by lhernand         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WOLF3D_H
+# define WOLF3D_H
+
+#include "libft/libft.h"
+#include "libft/get_next_line.h"
+#include "key_code.h"
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <math.h>
+#include <mlx.h>
+
+#endif
